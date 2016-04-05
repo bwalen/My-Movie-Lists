3f9f8766e372a1dddae776cac6f942897358300a
Brian Walen <bwalen@hotmail.com>
2016-04-05 13:10:19 -0700

    #3 I have implimented the front in elements and most of the back end to facilitate changing the name of the list.

--- public/js/default.js
+++ public/js/default.js
@@ -46,7 +46,12 @@ function processListData(e){
 function displayMovies(array){
   removeAll("movie-body");
   for(var i = 0 ; i < array.length ; i++){
-    getMovieData(array[i]);
+    if(array[i].title){
+      differentTitle(array[i].title);
+    }
+    else{
+      getMovieData(array[i]);
+    }
   }
 }
 
@@ -158,6 +163,10 @@ function displayOneMovie(inputObj){
 }
 
 function whichProfile(e){
+  removeAll("movie-body");
+  for(var i = 0; i < movieDataArray.length; i++){
+    displayOneMovie(movieDataArray[i]);
+  }
   var movieDisplay = document.getElementById("movie-body");
   var imdb = e.target.getAttribute("data-id");
   for (var i = 0; i < movieDataArray.length; i++){
@@ -350,17 +359,50 @@ function sorter(e){
   }
 }
 
+function changeTitle(){
+  var title = document.getElementById("title");
+  var inputText = document.getElementById("title-text");
+  var inputButton = document.getElementById("title-button");
+  title.setAttribute("class", "hidden");
+  inputText.setAttribute("class", "form-control");
+  inputButton.setAttribute("class", "btn btn-default");
+  inputButton.addEventListener("click", function(){
+    differentTitle(inputText.value);
+  });
+}
+
+function differentTitle(newTitleText){
+  var title = document.getElementById("title");
+  var inputText = document.getElementById("title-text");
+  var inputButton = document.getElementById("title-button");
+  var text = document.createTextNode(newTitleText);
+  inputText.setAttribute("class", "hidden form-control");
+  inputButton.setAttribute("class", "hidden btn btn-default");
+  title.setAttribute("class", "");
+  title.removeChild(title.firstChild);
+  title.appendChild(text);
+  var listTitle = {title: newTitleText};
+  for(var i = 0; i < moviesArray.length; i++){
+    if(_.isObject(moviesArray[i])){
+      moviesArray = _.without(moviesArray, moviesArray[i]);
+    }
+  }
+  moviesArray.push(listTitle);
+  document.cookie = ("list=" + JSON.stringify(moviesArray));
+}
+
 var searchButton = document.getElementById("search-button");
 var copyButton = document.getElementById("copy-button");
 var sortList = document.getElementById("sort-list");
 var topLists = document.getElementById("imdb-fifty");
 var rottenLists = document.getElementById("rotten-fifty");
-var detailsPage;
+var changeText = document.getElementById("change-title");
 searchButton.addEventListener("click", search);
 copyButton.addEventListener("click", copySession);
 sortList.addEventListener("click", sorter);
 topLists.addEventListener("click", function(){ displayMovieLists(imdbTopFifty)});
 rottenLists.addEventListener("click", function(){ displayMovieLists(tomatoesTopFifty)});
+changeText.addEventListener("click", changeTitle);
 var moviesArray = [];
 var movieDataArray = [];
 var imdbTopFifty = ["tt0111161","tt0068646","tt0071562","tt0468569","tt0108052","tt0050083","tt0110912","tt0167260","tt0120737","tt0060196","tt0137523","tt0080684","tt0109830","tt1375666","tt0167261","tt0073486","tt0099685","tt0133093","tt0047478","tt0076759","tt0317248","tt0114369","tt0102926","tt0038650","tt0114814","tt0118799","tt0110413","tt0064116","tt0245429","tt0120815","tt0816692","tt0034583","tt0120586","tt0021749","tt0054215","tt0082971","tt0047396","tt1675434","tt0027977","tt0120689","tt0103064","tt0253474","tt0407887","tt0088763","tt2582802","tt0209144","tt0172495","tt0078788","tt0482571","tt0057012"];
